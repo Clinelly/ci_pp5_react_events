367be0d8f070b3f2ab6838ae96adc82a05355a76
Clinelly <swall289@gmail.com>
2023-05-30 12:26:44 +0000

    Install and connect react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import Button from "react-bootstrap/Button";
 function App() {
   return (
     <div className="App">
       <h1>PP5 Events App</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
