258dcac9525842303fc6a138909625c126869d19
Clinelly <swall289@gmail.com>
2023-05-30 15:06:32 +0000

    Add routes to nav bar links and basic page content

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,43 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import Logo from "../assets/Logo.png";
-import styles from "../styles/NavBar.module.css"
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={Logo} alt="logo" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={Logo} alt="logo" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link><i className="fa-solid fa-house-chimney"></i>Home</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Sign In</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign Up</Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fa-solid fa-house-chimney"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fa-solid fa-right-to-bracket"></i>Sign In
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
+              <i className="fa-solid fa-user-plus"></i>Sign Up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
