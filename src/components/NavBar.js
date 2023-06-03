0a3469fa609acfa14f498af9cb55ee738cc11348
Clinelly <swall289@gmail.com>
2023-06-03 11:48:43 +0000

    Add logout functionality, create avater, update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,32 +3,92 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import Logo from "../assets/Logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedInIcons = (
-    <>{currentUser?.username}</>
-  )
-  const loggedOutIcons = (    
-    <> 
-      <NavLink
-      className={styles.NavLink}
-      activeClassName={styles.Active}
-      to="/signin"
-    >
-      <i className="fa-solid fa-right-to-bracket"></i>Sign In
-    </NavLink>
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+    
+  };
+  const addEventIcon = (
     <NavLink
       className={styles.NavLink}
       activeClassName={styles.Active}
-      to="/signup"
+      to="/events/create"
     >
-      <i className="fa-solid fa-user-plus"></i>Sign Up
-    </NavLink> 
-  </>
+      <i className="fa-solid fa-calendar-plus"></i>Create Event
+    </NavLink>
   );
-
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Event Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fa-regular fa-heart"></i>Liked
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/attending"
+      >
+        <i className="fa-regular fa-calendar-check"></i>Attending
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fa-solid fa-arrow-right-from-bracket"></i>Sign Out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar
+          src={currentUser?.profile_image}
+          text="Profile"
+          height={40}
+          width={40}
+        />
+      </NavLink>
+    </>
+  );
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fa-solid fa-right-to-bracket"></i>Sign In
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        <i className="fa-solid fa-user-plus"></i>Sign Up
+      </NavLink>
+    </>
+  );
 
   return (
     <Navbar className={styles.NavBar} expand="lg" fixed="top">
@@ -38,6 +98,7 @@ const NavBar = () => {
             <img src={Logo} alt="logo" />
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addEventIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -50,7 +111,6 @@ const NavBar = () => {
               <i className="fa-solid fa-house-chimney"></i>Home
             </NavLink>
             {currentUser ? loggedInIcons : loggedOutIcons}
-
           </Nav>
         </Navbar.Collapse>
       </Container>
