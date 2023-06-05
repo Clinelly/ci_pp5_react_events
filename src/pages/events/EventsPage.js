7d5d8253ec7522c6a1a23cecb99b7ea951570d3b
Clinelly <swall289@gmail.com>
2023-06-05 19:28:31 +0000

    Add functionality to display events list on homepage

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -4,40 +4,53 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Event from "./Event";
+import Asset from "../../components/Asset";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/EventsPage.module.css";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 
+import NoResults from "../../assets/no-results.png";
+
 function EventsPage(message, filter = "") {
   const [events, setEvents] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const {data} = await axiosReq.get(`/events/?${filter}`)
-        setEvents(data)
-        setHasLoaded(true)
-      } catch(err){}
-    }
-    setHasLoaded(false)
-    fetchEvents()
-  }, [filter, pathname])
+        const { data } = await axiosReq.get(`/events/?${filter}`);
+        setEvents(data);
+        setHasLoaded(true);
+      } catch (err) {}
+    };
+    setHasLoaded(false);
+    fetchEvents();
+  }, [filter, pathname]);
 
-  
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
         {hasLoaded ? (
           <>
-          {events.results.length ? (console.log("Map over posts and render")) : (console.log("Show no results"))}
+            {events.results.length ? (
+              events.results.map((event) => (
+                <Event key={event.id} {...event} setEvents={setEvents} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
           </>
         ) : (
-          console.log("Show spinner")
+          <Container>
+            <Asset spinner/>
+          </Container>
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
