e1c462e44a610d7f926e32e791cb6558bbb749c4
Clinelly <swall289@gmail.com>
2023-06-07 11:54:17 +0000

    Fix imports for EventPage.js

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -12,6 +12,9 @@ import { axiosReq } from "../../api/axiosDefaults";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function EventPage() {
   const { id } = useParams();
