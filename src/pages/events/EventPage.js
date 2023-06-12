01c38f5325dd850e6adbfebe1ccaddb8fe569f80
Clinelly <swall289@gmail.com>
2023-06-12 20:14:39 +0000

    Display popular profiles on event page

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -15,6 +15,7 @@ import Comment from "../comments/Comment";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function EventPage() {
   const { id } = useParams();
@@ -43,7 +44,7 @@ function EventPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+        <PopularProfiles mobile />
         <Event {...event.results[0]} setEvents={setEvent} eventPage />
         <Container className={appStyles.Content}>
           {currentUser ? (
@@ -80,7 +81,7 @@ function EventPage() {
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <PopularProfiles />
       </Col>
     </Row>
   );
