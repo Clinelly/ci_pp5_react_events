f30fad41b6bc4d8ba32883d3995728d83c5d3eaf
Clinelly <swall289@gmail.com>
2023-06-14 10:07:30 +0000

    Add basic footer to homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../App.module.css";
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container className={styles.Footer}>
+      <div>
+        <div>
+          <p>Developed by: Stuart Wall</p>
+        </div>
+        <div>
+          <a href="https://github.com/Clinelly">
+            <i className="fa-brands fa-square-github" />
+          </a>
+          <a href="https://www.linkedin.com/in/swall289/">
+            <i className="fa-brands fa-linkedin" />
+          </a>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Footer;
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import HomeCarousel from '../../components/HomeCarousel'
+import Footer from '../../components/Footer'
 
 const HomePage = () => {
   return (
     <div>
+        <h1>Event Horizon</h1>
+        <p>Your events, on the horizon.</p>
         <HomeCarousel />
+        <Footer />
     </div>
   )
 }
